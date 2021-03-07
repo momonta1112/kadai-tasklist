2a5c3e4b44edcf45f6781ef16c5dc2adcc38476f
momonta <nakao.subaka@gmail.com>
2021-03-07 14:04:35 +0000

    tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -30,12 +30,10 @@ class TasksController extends Controller
     public function create()
     {
         $task = new Task;
-        if (\Auth::id() === $task->user_id) {
+        
             return view('tasks.create', [
                 'task' => $task,
             ]);
-        }
-        return redirect('/');
     }
 
     // postでtasks/にアクセスされた場合の「新規登録処理」
